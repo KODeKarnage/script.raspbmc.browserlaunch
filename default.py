60bee6cf4ddb1dfbbd914e2c301c073a8c49a3cf
KODeKarnage <sublime.kev@gmail.com>
2013-11-26 18:07:35 +1100

    browser launch code

--- default.py
+++ default.py
@@ -19,12 +19,85 @@
 #  http://www.gnu.org/copyleft/gpl.html
 
 import os
-#import xbmcaddon
+import urllib2
+import xbmcgui
+import sys
+import hashlib
 
-#__addon__		= xbmcaddon.Addon("script.raspbmc.browserlaunch")
-#__scriptPath__	= __addon__.getAddonInfo('path')
-#your_script	= os.path.join(__scriptPath__, 'resources', 'your_script_name')
+
+
+live_ver_url         = 'http://svn.stmlabs.com/svn/raspbmc/release/update-system/browserver'
+local_ver_loc        = '/scripts/upd_hist/browserver'
+browser_download_url = 'download.raspbmc.com/downloads/bin/browser/browser.tar.gz'
+browser_md5_url      = 'download.raspbmc.com/downloads/bin/browser/browser.md5'
+prog_bar             = xbmcgui.DialogProgress()
+dialog               = xbmcgui.Dialog()
+
+
+def Launch():
+
+	#check version against source
+	live_ver = urllib2.urlopen(live_ver_url).read()
+	with open(local_ver_loc,'r') as f:
+		my_ver = f.read()
+
+	if not os.path.exists('/boot/browser.rfs') or my_ver == '':
+		app_verb = 'Installing...'
+		download_file(app_verb)
+	elif live_ver > my_ver:
+		app_verb = 'Updating...'
+		download_file(app_verb)
+	else:
+		launch_browser()
+
+
+def download_file(app_verb):
+
+	prog_bar.create("Arora Browser","Initializing...")
+	prog_bar.update(0,app_verb)
+
+	while not prog_bar.isCanceled():
+
+		file_name    = os.join('/tmp/',browser_download_url.split('/')[-1])
+		u            = urllib2.urlopen(browser_download_url)
+		f            = open(file_name, 'wb')
+		meta         = u.info()
+		file_size    = int(meta.getheaders("Content-Length")[0])
+		file_size_dl = 0
+		block_sz     = 8192
+
+		while True:
+		    buffer = u.read(block_sz)
+		    if not buffer:
+		        break
+
+		    file_size_dl += len(buffer)
+		    f.write(buffer)
+		    status = file_size_dl * 100. / file_size
+		    prog_bar.update(status,app_verb)
+
+		f.close()
+
+		#check md5 of downloaded file
+		prog_bar.update(100,'Verifying file...')
+		my_md5 = hashlib.md5(open(file_name, 'rb').read()).digest()
+		source_md5 = urllib2.urlopen(browser_md5_url).read()
+
+		if my_md5 != source_md5:
+			#continue without update
+			prog_bar.close()
+			launch_browser()
+		else:
+			#update or install browser from download
+			pass
+
+		break
+
+def launch_browser():
+	#code to launch the browser
+	os.system('bash /scripts/launch_browser.sh')
+
 
 if __name__ == "__main__":
-#	os.system('sh %s' % your_script)
-    os.system('sh /foo/bar.sh')
+	Launch()
+    
